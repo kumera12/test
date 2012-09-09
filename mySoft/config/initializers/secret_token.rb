8123caf4069f09ae8d9505e3ca4f71623e0871cd
Kumera Bekele <kumera12@yahoo.com>
2012-09-09 16:09:09 -0400

    initial commit

---
+++ mySoft/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MySoft::Application.config.secret_token = '9d437eef4251bd2c1545770591def457a47751827e1fa239b7b72915fa44e104a47161590fb4410239ac215afb0d919833d42fa1e8eeb4bafc4843e319163f1d'
